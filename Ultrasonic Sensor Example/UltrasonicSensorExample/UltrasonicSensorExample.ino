5d4d03eff7ac2e86436d793f063ba4f9d5eb42d2
M-Bellamy-0 <mbellamy2003@gmail.com>
2025-07-08 15:52:46 -0400

    Create UltrasonicSensorExample.ino

---
+++ Ultrasonic Sensor Example/UltrasonicSensorExample/UltrasonicSensorExample.ino
@@ -0,0 +1,78 @@
+//Sourced From: https://gist.github.com/dhruvsaidava/661acb770fb10ab0f7400d12cb5c0525
+// https://content.instructables.com/FRW/614F/H994I1VE/FRW614FH994I1VE.txt?_gl=1*k5ec1d*_ga*MTQwNjkxNTA0MC4xNzE5ODYyNDA1*_ga_NZSJ72N6RX*czE3NTA3NzkzOTYkbzQkZzEkdDE3NTA3NzkzOTYkajYwJGwwJGgw
+
+// Pin assignments
+const int trigPin = 22;    // Pin for sending trigger signal
+const int echoPin = 24;    // Pin for receiving echo signal
+const int redPin = 9;      // Pin for red LED
+const int yellowPin = 10;  // Pin for yellow LED
+const int greenPin = 11;   // Pin for green LED
+const int bluePin = 12;    // Pin for blue LED
+
+// Variables for storing distance and LED color
+long duration, distance;
+
+void setup() {
+  // Set pin modes
+  delay(5000);
+  Serial.print(__FILE__);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  pinMode(yellowPin, OUTPUT);
+  digitalWrite(redPin, LOW);
+  digitalWrite(greenPin, LOW);
+  digitalWrite(bluePin, LOW);
+  digitalWrite(yellowPin, LOW);
+}
+
+void loop() {
+  // Send trigger signal
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Measure duration of echo signal
+  duration = pulseIn(echoPin, HIGH);
+
+  // Calculate distance
+  distance = (duration / 2) / 29.1;
+
+  // Set LED color based on distance
+  if (distance < 10) {
+    digitalWrite(redPin, HIGH);
+    digitalWrite(yellowPin, HIGH);
+    digitalWrite(greenPin, HIGH);
+    digitalWrite(bluePin, HIGH);
+
+    delay(500);
+  } else if (distance < 20) {
+    digitalWrite(redPin, LOW);
+    digitalWrite(yellowPin, HIGH);
+    digitalWrite(greenPin, HIGH);
+    digitalWrite(bluePin, HIGH);
+    delay(500);
+  } else if (distance < 30) {
+    digitalWrite(redPin, LOW);
+    digitalWrite(yellowPin, LOW);
+    digitalWrite(greenPin, HIGH);
+    digitalWrite(bluePin, HIGH);
+    delay(500);
+  } else if (distance < 40) {
+    digitalWrite(redPin, LOW);
+    digitalWrite(yellowPin, LOW);
+    digitalWrite(greenPin, LOW);
+    digitalWrite(bluePin, HIGH);
+    delay(500);
+  } else {
+    digitalWrite(redPin, LOW);
+    digitalWrite(yellowPin, LOW);
+    digitalWrite(greenPin, LOW);
+    digitalWrite(bluePin, LOW);
+    delay(500);
+  }
+}
